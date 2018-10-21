8c80cd072ed4888037fdd4540747d2bc210279cf
Bhavana KV <kopparapubhavana@gmail.com>
2018-10-21 17:07:41 +0530

    Added mysql

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
+const  mysql = require("mysql");
 app.use(express.static(__dirname + '/public'));
+
+var con;
+function connection() {
+    con = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'root123',
+        database:'website'
+    }); 
+    con.connect(function(err) {
+        if(err) throw err;
+            console.log("Connected!");
+    }); 
+}
+connection();
 var port = 8000;
 app.listen(port);
 console.log("server on "+port);
